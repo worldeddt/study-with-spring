92e198915a73c656be8a3d4e0aa5df100b8fd942
김태형 <eddy@gimtaehyeong-ui-MacBookPro.local>
2023-12-11 03:12:54 +0900

    FEAT: one to one videl call 추가

---
+++ kurento-app/one-to-one-video-call/src/main/java/kurento/onetoonevideocall/OneToOneVideoCallApplication.java
@@ -0,0 +1,13 @@
+package kurento.onetoonevideocall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneVideoCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOneVideoCallApplication.class, args);
+    }
+
+}
---
+++ kurento-app/one-to-one-video-call/src/test/java/kurento/onetoonevideocall/OneToOneVideoCallApplicationTests.java
@@ -0,0 +1,13 @@
+package kurento.onetoonevideocall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneVideoCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
