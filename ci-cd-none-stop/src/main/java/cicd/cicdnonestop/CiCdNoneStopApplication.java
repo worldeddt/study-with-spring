fc7b028341cb0e6889313d15f9f673e945f4e0ff
worldeddt <ktest92@gmail.com>
2023-12-11 18:31:09 +0900

    CHORE: git actions 토이 프로젝트

---
+++ ci-cd-none-stop/src/main/java/cicd/cicdnonestop/CiCdNoneStopApplication.java
@@ -0,0 +1,13 @@
+package cicd.cicdnonestop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdNoneStopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdNoneStopApplication.class, args);
+    }
+
+}
---
+++ ci-cd-none-stop/src/test/java/cicd/cicdnonestop/CiCdNoneStopApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd.cicdnonestop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdNoneStopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
