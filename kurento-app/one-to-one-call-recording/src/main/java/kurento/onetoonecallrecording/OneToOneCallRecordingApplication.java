8a33c2a4b92331ccbd997b0cec567b98964d6444
worldeddt <ktest92@gmail.com>
2023-12-12 17:48:17 +0900

    FEAT:(NONE) 화면 recording 버전

---
+++ kurento-app/one-to-one-call-recording/src/main/java/kurento/onetoonecallrecording/OneToOneCallRecordingApplication.java
@@ -0,0 +1,63 @@
+package kurento.onetoonecallrecording;
+
+import lombok.extern.slf4j.Slf4j;
+import org.kurento.client.KurentoClient;
+import org.kurento.client.KurentoClientBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+
+@Slf4j
+@EnableWebSocket
+@SpringBootApplication
+public class OneToOneCallRecordingApplication implements WebSocketConfigurer {
+
+    @Bean
+    public CallHandler callHandler() {
+        return new CallHandler();
+    }
+
+    @Bean
+    public UserRegistry registry() {
+        return new UserRegistry();
+    }
+
+    @Bean
+    public KurentoClient kurentoClient() {
+        return new KurentoClientBuilder()
+                .setKmsWsUri("ws://localhost:8888/kurento")
+                .onDisconnected(
+                () -> {
+                    log.info("disconnect !!!");
+                }
+        ).onConnected(
+                () -> {
+                    log.info("connect success!!!");
+                }
+        ).connect();
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(32768);
+        return container;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(callHandler(), "/call")
+                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOneCallRecordingApplication.class, args);
+    }
+
+}
---
+++ kurento-app/one-to-one-call-recording/src/test/java/kurento/onetoonecallrecording/OneToOneCallRecordingApplicationTests.java
@@ -0,0 +1,13 @@
+package kurento.onetoonecallrecording;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneCallRecordingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
