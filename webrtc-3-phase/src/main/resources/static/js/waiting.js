4c7e3e2efcbf8db33223e4b0451ef1d83b412dc1
worldeddt <ktest92@gmail.com>
2023-12-19 10:39:29 +0900

    대기방 리스트 비동기 처리 및 html 추가 수정

--- webrtc-3-phase/src/main/resources/static/js/waiting.js
+++ webrtc-3-phase/src/main/resources/static/js/waiting.js
@@ -1,37 +1,49 @@
-
 const ROOMCODE = "ferimRoom";
+const HOST = "https://localhost:8081"
+const ROOM_ALL_URI = "/room/all"
 let roomList = [];
 
-const apiRequestGet = (_url) => {
-    axios.get(_url)
-        .then(function (response) {
-
-            window.roomList = roomList;
-            response.data.list.map(property => {
-                roomList.push(JSON.parse(property));
-            })
-
-            settingListBody();
-
-        }).catch(function(reason) {
-            console.log("reason", reason);
-    });
+async function apiRequestGet(_url) {
+  return await axios.get(_url);
 }
+
+async function apiRequestPost(_url, _data) {
+  return await axios.post(_url, _data)
+}
+
+(function () {
+  const createRoomButton = document.getElementById("room_create");
+
+  apiRequestGet(`${HOST}${ROOM_ALL_URI}/${ROOMCODE}`)
+    .then(function (response) {
+      window.roomList = roomList;
+      response.data.list.map(property => {
+          roomList.push(JSON.parse(property));
+      });
+
+
+    }).catch(function (reason) {
+      console.log("reason", reason);
+    }).finally(function (e) {
+      settingListBody();
+  });
+
+    createRoomButton.addEventListener("click", function(e) {
+        console.log(e);
+    })
+})();
 
 function settingListBody() {
     const tbody = document.getElementById("room-body");
 
     for (room of roomList) {
-        console.log(room);
-        let tr= document.createElement('tr');
-        let td= document.createElement('td');
+      let tr= document.createElement('tr');
+      let titleTd= document.createElement('td');
+      let peopleNumberTd = document.createElement("td");
 
-        td.innerHTML =  room.roomName;
-        tr.append(td);
+      titleTd.innerHTML =  room.roomName;
+      tr.append(titleTd);
 
-        tbody.append(tr);
+      tbody.append(tr);
     }
 }
-
-
-apiRequestGet(`https://localhost:8081/room/all/${ROOMCODE}`);
