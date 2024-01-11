c43faa11226dd85a17cd1da378fe728dc536d083
worldeddt <ktest92@gmail.com>
2024-01-11 18:40:52 +0900

    feat: redis overcommit memory 직전 까지

--- redis-cluster/redis/redis-7.2.3/entrypoint.sh
+++ redis-cluster/redis/redis-7.2.3/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-touch test.sh
+echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf
+#echo 1 >/proc/sys/vm/overcommit_memory
+sudo sysctl vm.overcommit_memory=1
+redis-server /redis/redis.conf
