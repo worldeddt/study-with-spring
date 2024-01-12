52c5cadfa4d13391ccc1608138408f6e06093017
worldeddt <ktest92@gmail.com>
2024-01-12 18:37:44 +0900

    feat: redis cluster각 노드 띄우는 것 까지 성공

--- redis-cluster/redis/redis-7.2.3/entrypoint.sh
+++ redis-cluster/redis/redis-7.2.3/entrypoint.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf
+#chmod 777 /etc/sysctl.conf
+#echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf
+#sudo sysctl -p
 #echo 1 >/proc/sys/vm/overcommit_memory
-sudo sysctl vm.overcommit_memory=1
-redis-server /redis/redis.conf
+#sudo sysctl vm.overcommit_memory=1
+
+redis-server /redis/redis.conf --daemonize yes
+tail -f /dev/null
