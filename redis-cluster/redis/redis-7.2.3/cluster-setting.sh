9530292a6c884cd3e005b84ab8526c3f8e027a02
worldeddt <ktest92@gmail.com>
2024-01-18 18:36:49 +0900

    feat: cluster mesh 토폴로지 초안

---
+++ redis-cluster/redis/redis-7.2.3/cluster-setting.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+redis-cli --cluster create 20.214.111.167:6300 20.214.111.167:6301 20.214.111.167:6302 20.214.181.249:6300 20.214.181.249:6301 20.214.181.249:6302
+redis-cli --cluster add-node 20.214.181.249:6400 20.214.111.167:6300 --cluster-slave
+redis-cli --cluster add-node 20.214.181.249:6400 20.214.111.167:6301 --cluster-slave
+redis-cli --cluster add-node 20.214.181.249:6400 20.214.181.249:6300 --cluster-slave
+
+redis-cli --cluster add-node 20.214.181.249:6401 20.214.111.167:6300 --cluster-slave
+redis-cli --cluster add-node 20.214.181.249:6401 20.214.111.167:6302 --cluster-slave
+redis-cli --cluster add-node 20.214.181.249:6401 20.214.181.249:6301 --cluster-slave
+
+redis-cli --cluster add-node 20.214.181.249:6402 20.214.111.167:6301 --cluster-slave
+redis-cli --cluster add-node 20.214.181.249:6402 20.214.111.167:6302 --cluster-slave
+redis-cli --cluster add-node 20.214.181.249:6402 20.214.181.249:6302 --cluster-slave
+
+redis-cli --cluster add-node 20.214.111.167:6400 20.214.181.249:6300 --cluster-slave
+redis-cli --cluster add-node 20.214.111.167:6400 20.214.181.249:6302 --cluster-slave
+redis-cli --cluster add-node 20.214.111.167:6400 20.214.111.167:6300 --cluster-slave
+
+redis-cli --cluster add-node 20.214.111.167:6401 20.214.181.249:6300 --cluster-slave
+redis-cli --cluster add-node 20.214.111.167:6401 20.214.181.249:6301 --cluster-slave
+redis-cli --cluster add-node 20.214.111.167:6401 20.214.111.167:6301 --cluster-slave
+
+redis-cli --cluster add-node 20.214.111.167:6402 20.214.181.249:6301 --cluster-slave
+redis-cli --cluster add-node 20.214.111.167:6402 20.214.181.249:6302 --cluster-slave
+redis-cli --cluster add-node 20.214.111.167:6402 20.214.111.167:6302 --cluster-slave
