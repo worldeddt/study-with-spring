21532c3e42269e311b852d8b86199b1cac4d2050
worldeddt <ktest92@gmail.com>
2024-02-09 09:55:59 +0900

    feat: resource 코드 적용

--- aop/src/main/java/aop/prototypes/thread/deadlock/DeadLockExample.java
+++ aop/src/main/java/aop/prototypes/thread/deadlock/DeadLockExample.java
@@ -8,6 +8,21 @@ public class DeadLockExample {
 
 
     public static void main(String[] args) {
+
+        Resource resource1 = new Resource("Resource 1");
+        Resource resource2 = new Resource("Resource 2");
+
+        Thread thread3 = new Thread(() -> {
+            resource1.method(resource2);
+        });
+
+        Thread thread4 = new Thread(() -> {
+            resource2.method(resource1);
+        });
+
+        thread3.start();
+        thread4.start();
+
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
