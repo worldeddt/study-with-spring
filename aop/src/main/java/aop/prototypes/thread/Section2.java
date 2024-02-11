0d0c291b06a5e2b2e4e3bb3100f630ed5b906eff
worldeddt <ktest@gmail.com>
2024-02-11 12:47:31 +0900

    feat: 가시성

--- aop/src/main/java/aop/prototypes/thread/Section2.java
+++ aop/src/main/java/aop/prototypes/thread/Section2.java
@@ -1,2 +1,40 @@
-package aop.prototypes.thread;public class Section2 {
+package aop.prototypes.thread;
+
+import aop.prototypes.MainApplication;
+import kotlin.jvm.Volatile;
+import org.springframework.boot.SpringApplication;
+
+import java.io.Console;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Section2 {
+
+
+    private static boolean shouldStop = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Section2.class, args);
+task();
+
+    }
+
+    private static void task() throws InterruptedException {
+        Thread thread1 = new Thread(Section2::doWork);
+        thread1.start();
+        System.out.printf("start");
+        TimeUnit.SECONDS.sleep(3);
+        shouldStop = true;
+        thread1.join();
+        System.out.printf("end");
+    }
+
+    private static void doWork() {
+
+        boolean toggle = false;
+
+        while (shouldStop == false) {
+            toggle = !toggle;
+        }
+    }
 }
