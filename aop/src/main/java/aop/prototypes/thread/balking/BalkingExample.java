0746178076410f3c020f2d7d9a40d11ba33d69f8
worldeddt <ktest92@gmail.com>
2024-02-13 10:46:03 +0900

    feat: balking pattern

---
+++ aop/src/main/java/aop/prototypes/thread/balking/BalkingExample.java
@@ -0,0 +1,12 @@
+package aop.prototypes.thread.balking;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalkingExample {
+
+    public static void main(String[] args) {
+
+    }
+}
