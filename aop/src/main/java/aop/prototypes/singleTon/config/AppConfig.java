fa55d40d8fe0a7d27d880b559024ece1882487dc
worldeddt <ktest92@gmail.com>
2024-03-04 09:36:20 +0900

    feat: appconfig

--- aop/src/main/java/aop/prototypes/singleTon/config/AppConfig.java
+++ aop/src/main/java/aop/prototypes/singleTon/config/AppConfig.java
@@ -9,12 +9,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
 
     @Bean
     public MemberRepository memberRepository() {
