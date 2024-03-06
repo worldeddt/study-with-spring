f9ed9c9adb00881b1cf9f0267ed920268d96eb7e
worldeddt <ktest92@gmail.com>
2024-03-06 14:42:19 +0900

    fix - 날짜 포맷터 추가

---
+++ aop/src/main/java/aop/prototypes/기타문법/GrammerExample.java
@@ -0,0 +1,28 @@
+package aop.prototypes.기타문법;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@SpringBootApplication
+public class GrammerExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrammerExample.class, args);
+        convert();
+    }
+
+    static void convert() {
+        String str = "2024-03-19 13:34:04.996977";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        LocalDateTime dateTime = LocalDateTime.parse(str, formatter);
+        System.out.println( "hour : "+ dateTime.getHour());
+        System.out.println( "minute : "+ dateTime.getMinute());
+        System.out.println( "second : "+ dateTime.getSecond());
+    }
+
+}
