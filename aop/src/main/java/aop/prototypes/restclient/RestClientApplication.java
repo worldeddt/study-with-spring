9e617a2249401c8320210522deebf97bd3644668
worldeddt <ktest92@gmail.com>
2024-03-12 09:28:12 +0900

    feat: rest client test

---
+++ aop/src/main/java/aop/prototypes/restclient/RestClientApplication.java
@@ -0,0 +1,14 @@
+package aop.prototypes.restclient;
+
+
+import aop.prototypes.MainApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientApplication.class, args);
+    }
+}
