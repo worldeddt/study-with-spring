6594c7cad9f81cb19e4b5333287ff1c9ffb8331f
worldeddt <ktest92@gmail.com>
2024-03-14 14:59:36 +0900

    fix - 날짜 형식 formatter 추가

--- aop/src/main/java/aop/prototypes/기타문법/GrammerExample.java
+++ aop/src/main/java/aop/prototypes/기타문법/GrammerExample.java
@@ -4,6 +4,8 @@ package aop.prototypes.기타문법;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -20,6 +22,10 @@ public class GrammerExample {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
         LocalDateTime dateTime = LocalDateTime.parse(str, formatter);
+
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("date whole : "+dateTime.format(formatter2));
         System.out.println( "hour : "+ dateTime.getHour());
         System.out.println( "minute : "+ dateTime.getMinute());
         System.out.println( "second : "+ dateTime.getSecond());
