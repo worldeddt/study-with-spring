851c9ef9813b825e7fcd093cc6651fc942489dca
worldeddt <ktest92@gmail.com>
2024-03-15 18:31:17 +0900

    feat: kafka 기본 서비스 2개

---
+++ aop/src/main/java/aop/prototypes/kafka/service1/ServiceExample1.java
@@ -0,0 +1,14 @@
+package aop.prototypes.kafka.service1;
+
+
+import aop.prototypes.logBack.LogBackApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceExample1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceExample1.class, args);
+    }
+}
---
+++ aop/src/main/java/aop/prototypes/kafka/service2/ServiceExample2.java
@@ -0,0 +1,14 @@
+package aop.prototypes.kafka.service2;
+
+import aop.prototypes.kafka.service1.ServiceExample1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ServiceExample2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceExample2.class, args);
+    }
+}
