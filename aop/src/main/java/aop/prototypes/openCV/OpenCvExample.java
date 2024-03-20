5f55783348eccb33de0e5d4e69ac7c0668c16631
worldeddt <ktest92@gmail.com>
2024-03-20 18:17:06 +0900

    feat: prometheus 연동 코드

---
+++ aop/src/main/java/aop/prototypes/openCV/OpenCvExample.java
@@ -0,0 +1,12 @@
+package aop.prototypes.openCV;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCvExample {
+
+    public static void main(String[] args) {
+
+    }
+}
