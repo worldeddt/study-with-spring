ef1983dcdaa3350ab935d3ad942e7c671a019a28
worldeddt <ktest92@gmail.com>
2024-10-28 13:53:07 +0900

    feat: media

--- aop/src/main/java/aop/prototypes/mapper/MediaExample.java
+++ aop/src/main/java/aop/prototypes/mapper/MediaExample.java
@@ -1,8 +1,6 @@
-package aop.prototypes.media;
+package aop.prototypes.mapper;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 //@SpringBootApplication
